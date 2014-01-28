e404ac117a607503b511fe8ab61f707484217fbc
Tatsu Ikeda <tatsu@tikeda.com>
2014-01-28 13:47:29 -0500

    added table view controller class

---
+++ ToDoList2/TITodoListViewController.h
@@ -0,0 +1,13 @@
+//
+//  TITodoListViewController.h
+//  ToDoList2
+//
+//  Created by Tatsu Ikeda on 1/28/14.
+//  Copyright (c) 2014 Tatsu Ikeda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TITodoListViewController : UITableViewController
+
+@end
